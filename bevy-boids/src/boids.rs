21495bc23cfae6d2253c438d72f7a151889cccc6
JacksonBowe <jackson.bowe@powertec.com.au>
2023-02-02 08:04:58 +1000

    got one kinda working

--- bevy-boids/src/boids.rs
+++ bevy-boids/src/boids.rs
@@ -1,26 +1,51 @@
 use bevy::prelude::*;
 
-// use rand::prelude::*;
+use rand::prelude::*;
 
 pub struct BoidsPlugin;
 impl Plugin for BoidsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(boids_system);
+        app.add_system(boids_system);
     }
 }
 
 #[derive(Component, Debug)]
 pub struct Boid {
-    pub velocity: Vec3,
-    pub position: Vec3,
+    pub velocity: Vec2,
+    pub position: Vec2,
     pub separation: f32,
     pub cohesion: f32,
     pub attraction: f32,
-    pub group: Option<String>
+    pub group: i32
 }
 
-fn boids_system() {
-    println!("Test");
+impl Default for Boid {
+    fn default() -> Self {
+        Boid {
+            velocity: Vec2::ZERO,
+            position: Vec2::ZERO,
+            separation: 0.1,
+            cohesion: 0.1,
+            attraction: 0.1,
+            group: 0
+        }
+    }
+}
+
+fn boids_system(mut boids: Query<(&mut Boid, &mut Transform)>) {
+    // Calculate aspects for the boid group
+    for (_boid, mut transform) in &mut boids {
+        println!("{}", transform.rotation);
+
+        let mut rng = rand::thread_rng();
+        let y: f64 = rng.gen(); // generates a float between 0 and 1
+        let turn_direction = y - 0.5;
+
+        transform.rotate_z(turn_direction as f32);
+
+        let test = transform.rotation;
+        transform.translation += test * Vec3::splat(1.)
+    }
 }
 
 
--- bevy-boids/src/main.rs
+++ bevy-boids/src/main.rs
@@ -2,6 +2,7 @@ use bevy::{
     prelude::*,
     sprite::MaterialMesh2dBundle
 };
+use boids::Boid;
 
 mod boids;
 
@@ -17,10 +18,15 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials
     // Spawn the camera
     commands.spawn(Camera2dBundle::default());
     commands.spawn(MaterialMesh2dBundle {
-        // mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
         mesh: meshes.add(Mesh::from(shape::RegularPolygon::new(0.1, 3))).into(),
-        transform: Transform::default().with_scale(Vec3::splat(128.)),
+        transform: Transform::default().with_scale(Vec3::splat(128.)).with_rotation(Quat::default()),
         material: materials.add(ColorMaterial::from(Color::PURPLE)),
         ..default()
-    });
+    }).insert(Boid::default());
 }
+
+// fn pointer_system(mut query: Query<&Transform>, mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterials>>) {
+//     for entity in &mut query {
+//         println!("{}", entity)
+//     }
+// }
