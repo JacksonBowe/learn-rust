74eb9352dbcad7e0b91a169ed281eca8abbf2092
Jackson Bowe <jacksonbowe98@gmail.com>
2023-02-02 12:17:24 +1000

    velocity system

--- bevy-boids/src/boids.rs
+++ bevy-boids/src/boids.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
-
 use rand::prelude::*;
+
+use super::Velocity;
 
 pub struct BoidsPlugin;
 impl Plugin for BoidsPlugin {
@@ -32,19 +33,50 @@ impl Default for Boid {
     }
 }
 
-fn boids_system(mut boids: Query<(&mut Boid, &mut Transform)>) {
+fn boids_system(mut boids: Query<(&mut Boid, &mut Transform, &mut Velocity)>) {
     // Calculate aspects for the boid group
-    for (_boid, mut transform) in &mut boids {
-        println!("{}", transform.rotation);
+    let mut positions = Vec3::ZERO;
+    let mut directions = Vec3::ZERO;
+    let mut num_boids = 0;
+    for (_boid, mut transform, mut velocity) in &mut boids {
+        // Add boid position to the positions Vecor
+        positions += transform.translation;
 
-        let mut rng = rand::thread_rng();
-        let y: f64 = rng.gen(); // generates a float between 0 and 1
-        let turn_direction = y - 0.5;
+        // Add boid direction to the directions Vector
+        directions.x += velocity.x;
+        directions.y += velocity.y;
 
-        transform.rotate_z(turn_direction as f32);
+        num_boids += 1;
 
-        let test = transform.rotation;
-        transform.translation += test * Vec3::splat(1.)
+    // Calculate the averages
+    let mut average_position = positions / num_boids as f32;
+    let mut average_direction = directions / num_boids as f32;
+
+    println!("AvP: {}", average_position);
+    println!("AvD: {}", average_direction);
+
+    // Adjust the velocity of each boid based on the averages
+
+    for (_boid, mut transform, mut velocity) in &mut boids {
+        velocity += average_position.normalize()
+    }
+    
+        // // Get the velocity vector
+        // println!("V: [{}, {}]", velocity.x, velocity.y);
+        // // Add boid position to the average
+
+        
+        // // Update translation.rotation to match new velocity vector
+        // println!("R: {}", transform.rotation);
+
+        // let mut rng = rand::thread_rng();
+        // let y: f64 = rng.gen(); // generates a float between 0 and 1
+        // let turn_direction = y - 0.5;
+
+        // transform.rotate_z(turn_direction as f32);
+
+        // let test = transform.rotation;
+        // transform.translation += test * Vec3::new(1., 1., 0.)
     }
 }
 
--- bevy-boids/src/main.rs
+++ bevy-boids/src/main.rs
@@ -11,18 +11,44 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(boids::BoidsPlugin)
         .add_startup_system(setup)
+        .add_system(apply_velocity)
         .run()
 }
+
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
 
 fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
     // Spawn the camera
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(Mesh::from(shape::RegularPolygon::new(0.1, 3))).into(),
-        transform: Transform::default().with_scale(Vec3::splat(128.)).with_rotation(Quat::default()),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        ..default()
-    }).insert(Boid::default());
+    commands.spawn((MaterialMesh2dBundle {
+            mesh: meshes.add(Mesh::from(shape::RegularPolygon::new(0.1, 3))).into(),
+            transform: Transform::default().with_scale(Vec3::splat(128.)).with_rotation(Quat::default()),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            ..default()
+        },
+        Boid::default(),
+        Velocity(Vec2::new(1.0, 1.0)),
+    ));
+    commands.spawn((MaterialMesh2dBundle {
+            mesh: meshes.add(Mesh::from(shape::RegularPolygon::new(0.1, 3))).into(),
+            transform: Transform::from_xyz(30., 0., 0.).with_scale(Vec3::splat(128.)).with_rotation(Quat::default()),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            ..default()
+        },
+        Boid::default(),
+        Velocity(Vec2::new(1.0, 1.0)),
+    ));
+}
+
+fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
+    let dt = time.delta_seconds();
+    let speed = 100.;
+    for (mut transform, velocity) in &mut query {
+        // println!("{}", transform.translation);
+        transform.translation.x += velocity.x * speed * dt;
+        transform.translation.y += velocity.y * speed * dt;
+    }
 }
 
 // fn pointer_system(mut query: Query<&Transform>, mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterials>>) {
